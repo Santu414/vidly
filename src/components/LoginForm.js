14b538aa768e96fa3dea2e70e4787351a7e6c2e1
Santu414 <snagankera@gmail.com>
2020-02-27 14:10:19 +0530

    Custom form validation for username and password

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,50 +1,77 @@
 import React, { Component } from "react";
+import Input from "./common/Input";
 
 class LoginForm extends Component {
   state = {
-    account: { username: "", password: "" }
+    account: { username: "", password: "" },
+    errors: {}
+  };
+
+  validate = e => {
+    const errors = {};
+    const { account } = this.state;
+    if (account.username.trim() === "") {
+      errors.username = "Username is required";
+    }
+
+    if (account.password.trim() === "") {
+      errors.password = "Password is required";
+    }
+    return Object.keys(errors).length === 0 ? null : errors;
   };
   handleSubmit = e => {
     e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) {
+      return;
+    }
+    //call the server
     console.log("Submitted");
   };
 
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required";
+    }
+
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required";
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login Form</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">User Name</label>
-            <input
-              onChange={this.handleChange}
-              value={this.state.account.username}
-              name="username"
-              type="email"
-              className="form-control"
-              id="username"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-          </div>
+          <Input
+            value={account.username}
+            name="username"
+            label="Username"
+            onChange={this.handleChange}
+            error={errors.username}
+          />
 
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={this.handleChange}
-              value={this.state.account.password}
-              name="password"
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Password"
-            />
-          </div>
+          <Input
+            value={account.password}
+            name="password"
+            label="Password"
+            onChange={this.handleChange}
+            error={errors.password}
+          />
 
           <button type="submit" className="btn btn-primary">
             Submit
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,19 +1,20 @@
 import React from "react";
 
-const Input = props => {
+const Input = ({ name, label, value, error, onChange }) => {
   return (
     <div className="form-group">
-      <label htmlFor="exampleInputEmail1">User Name</label>
+      <label htmlFor={name}>{label}</label>
       <input
-        onChange={this.handleChange}
-        value={this.state.account.username}
-        name="username"
-        type="email"
+        onChange={onChange}
+        value={value}
+        name={name}
+        type="text"
         className="form-control"
-        id="username"
+        id={name}
         aria-describedby="emailHelp"
         placeholder="Enter email"
       />
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
