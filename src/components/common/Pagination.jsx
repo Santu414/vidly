e44885f9aa0df2a6d195c38b1a0880813fa9950e
Santosh Reddy <snagankera@gmail.com>
2022-11-19 02:30:14 +0530

    Destructuring props

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import _ from "lodash";
 import PropTypes from "prop-types";
 
-const Pagination = (props) => {
-  const { itemCount, pageSize, onPageChange, currentPage } = props;
+const Pagination = ({ itemCount, pageSize, onPageChange, currentPage }) => {
   const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount === 1) return null;
   const pages = _.range(1, pageCount + 1);
