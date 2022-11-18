2f8858c6ec750a1d7574d595b59eaeaf75a2664c
Santosh Reddy <snagankera@gmail.com>
2022-11-19 04:05:50 +0530

    Added rouiting to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Movies from "./components/Movies";
+import Customers from "./components/Customers";
+import Rentals from "./components/Rentals";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <Routes>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="/customers" element={<Customers />}></Route>
+        <Route path="/rentals" element={<Rentals />}></Route>
+        <Route path="/not-found" element={<NotFound />}></Route>
+        <Route path="/" element={<Navigate to="/movies" />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </main>
   );
 }
