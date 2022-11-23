fea2bd7c1511240d937876ddcc3d836b9ae1bed1
Santosh Reddy <snagankera@gmail.com>
2022-11-23 13:21:40 +0530

    Refactoring the code

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,23 +2,24 @@ import http from "./httpService";
 import config from "../config.json";
 
 const apiEndPoint = config.apiUrl + "/movies";
-console.log(apiEndPoint);
-
+function movieURL(id) {
+  return `${apiEndPoint}/${id}`;
+}
 export function getMovies() {
   return http.get(apiEndPoint);
 }
 export function getMovie(movieId) {
-  return http.get(apiEndPoint + "/" + movieId);
+  return http.get(movieURL(movieId));
 }
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(apiEndPoint + "/" + movie._id, body);
+    return http.put(movieURL(movie._id), body);
   }
   return http.post(apiEndPoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndPoint + "/" + movieId);
+  return http.delete(movieURL(movieId));
 }
